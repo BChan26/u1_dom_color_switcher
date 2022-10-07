01a98f7c20879e96c6f18a1d3b6b8764e74bfc8c
Brandon Chan <bchan26@bu.edu>
2022-10-07 13:34:31 -0400

    finished classwork and took notes on DOM - check paper notes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-// Write your DOM code here!
+// red and white and blue and yellow
+
+//we're making this variable "red" to reference below
+let red = document.querySelector(`.red`)
+red.addEventListener('click', () => {
+    document.body.classList.toggle(`red`)})
+
+    let white = document.querySelector(`.white`)
+white.addEventListener('click', () => {
+    document.body.classList.toggle(`white`)})
+
+    let blue = document.querySelector(`.blue`)
+    blue.addEventListener('click', () => {
+        document.body.classList.toggle(`blue`)})
+
+        let yellow = document.querySelector(`.yellow`)
+yellow.addEventListener('click', () => {
+    document.body.classList.toggle(`yellow`)})
+
+let gradientColor = document.querySelector(`.gradientH`)
+gradientColor.addEventListener('click', () => {
+    document.body.classList.toggle(`gradientC`)})
+
+    let gradientColorG = document.querySelector(`.gradientG`)
+gradientColorG.addEventListener('click', () => {
+    document.body.classList.toggle(`gradientG`)})
